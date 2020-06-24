535060bfa6e1b449bceacbe4776cb20f4aa4be79
gouemoolaf28 <gouemoolaf@yahoo.fr>
2020-06-24 11:30:38 +0200

    third commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Ninjas from './components/ninjas'
+import Ninjas from './components/ninjas';
+import AddNinja from './components/AddNinja';
 
 class App extends Component {
   state = {
-    ninjas : [
-      {name: "Olaf", age: 30, belt: "Black", id: 1},
-      {name: "Regis", age: 20, belt: "Yellow", id: 2},
-      {name: "Honey", age: 25, belt: "green", id: 3}
+    ninjas: [
+      { name: "Olaf", age: 30, belt: "Black", id: 1 },
+      { name: "Regis", age: 20, belt: "Yellow", id: 2 },
+      { name: "Honey", age: 25, belt: "green", id: 3 }
     ]
   }
   render() {
@@ -14,7 +15,8 @@ class App extends Component {
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas = {this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }
--- myapp/src/components/ninjas.js
+++ myapp/src/components/ninjas.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Ninjas extends Component {
-  render() {
-    console.log(this.props);
-    const {ninjas} = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id }>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      )
-    });
-    return (
-      <div className="ninja-list"> 
-        {ninjaList}
+const Ninjas = (props) => {
+  console.log(props);
+  const { ninjas } = props;
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
       </div>
-    )
-  }
+    ) : null;
+  })
+  // const ninjaList = ninjas.map(ninja => {
+  //   if (ninja.age > 20) {
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: {ninja.name}</div>
+  //         <div>Age: {ninja.age}</div>
+  //         <div>Belt: {ninja.belt}</div>
+  //       </div>
+  //     )
+  //   } else {
+  //     return null;
+  //   }
+  // });
+  return (
+    <div className="ninja-list">
+      {ninjaList}
+    </div>
+  )
 }
 
 export default Ninjas;
